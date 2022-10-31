053127f2cc318c55e9acc2b1109fdb5a2d6d2838
ASZabavin <ASZabavin@pnu.edu.ru>
2022-10-31 12:56:14 +1000

    fix

--- src/lab2/main.py
+++ src/lab2/main.py
@@ -1,5 +1,111 @@
+import random
+import numpy as np
+import pandas as pd
+import math
+import itertools
+from numpy.core.records import ndarray
+
+np.random.seed(42)
+
+
+class Image:
+    """
+    Образ с произвольным набором признаков
+    """
+
+    def __init__(self, **features):
+        """
+        features: распаковка набора признаков со значениями, значения должные быть действительными числами
+        """
+        for key, val in features.items():
+            if not isinstance(val, (int, float, ndarray)):
+                raise ValueError('Значения признаков должны быть действительными числами')
+            setattr(self, key, val)
+
+        self._dimensionality = len(features)
+        self._features_names = features.keys()
+
+    @property
+    def dimensionality(self):
+        return self._dimensionality
+
+    @property
+    def features_names(self):
+        return self._features_names
+
+
+class ClassNormalCloud:
+    """
+    Облако образов в пространстве признаков
+    """
+
+    def __init__(self, N, **Md_ft):
+        """
+        :param N: размер облака
+
+        Md_ft: распаковка параметров для нормального распределения признаков
+            Md_ft['x'] =
+            'M': float, # математическое ожидание признака x
+            'D': float, # дисперсия случайной величины признака x
+            }
+            Md_ft['y'] =
+            'M': float, # математическое ожидание признака y
+            'D': float, # дисперсия случайной величины признака y
+            }
+
+            ...
+            Md_ft['n'] =
+            'M': float, # математическое ожидание признака n
+            'D': float, # дисперсия случайной величины признака n
+            }
+
+        """
+        self._features_names = []
+        self._dimensionality = 0
+        self._size = N
+        self._images = []
+
+        for key, val in Md_ft.items():
+            if not isinstance(key, (str, int)):
+                raise ValueError('Наименование признака должно быть строкой или натуральным числом')
+            if not isinstance(val, dict) or ('M' not in val) or ('D' not in val):
+                raise ValueError(f'Параметры облака образа признака {key} должны быть словарем, содержащим как минимум ключи M и D')
+
+            # все норм
+            self._features_names.append(key)
+            self._dimensionality += 1
+            setattr(self, key, {**val})
+
+    @property
+    def dimensionality(self):
+        return self._dimensionality
+
+    @property
+    def features_names(self):
+        return self._features_names
+
+    @property
+    def size(self):
+        return self._size
+
+    def fill_cloud(self):
+
+        features_appropriate = []
+        for key in self.features_names:
+            fsett = getattr(self, key)
+            sko = math.sqrt(fsett['D'])
+            features_appropriate.append(np.nditer(np.random.normal(fsett['M'], sko, self.size)))  # fsett['M']-матожидание величины признака sko-СКО  self.size.-размер массива
+
+        for i, features in enumerate(itertools.zip_longest(*features_appropriate)):
+            ftu = {k: v for k, v in itertools.zip_longest(self.features_names, features)}
+            self._images.append(Image(**ftu))
+            pass
+
+        pass
 
 
 if __name__ == "__main__":
-
+    cloud1 = ClassNormalCloud(100, x={'M': 1000, 'D': 100}, y={'M': 1000, 'D': 100})
+    cloud1.fill_cloud()
+    gg = ''
     pass
